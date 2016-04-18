c82bb6aa6bf35f83846a2ebb803f8226f58ea14d
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-04-18 14:57:44 +0800

    Initial migration to markdown-it

--- index.js
+++ index.js
@@ -18,30 +18,85 @@ module.exports = function styledown (files, options) {
 }
 
 function buildFile (data) {
-  var tokens = marked.lexer(data)
-  var result = {
-    name: null,
-    sections: {}
-  }
-  var section
+  var md = require('markdown-it')()
+  var env = {}
+  var result = {}
 
-  tokens.forEach(function (t) {
-    // h1
-    if (t.type === 'heading' && t.depth === 1) {
-      result.name = t.text
-    }
-
-    // h1...h6 { type: 'heading', depth: 3, text: 'header' }
-    if (t.type === 'heading') {
-      var id = slugify(t.text)
-      section = { type: 'text', depth: t.depth, heading: t.text }
-      result.sections[id] = section
-    }
-  })
-
+  var tokens = [].concat(md.parse(data, env))
+  eatDocument(md, tokens, result)
   return result
 }
 
-function slugify (str) {
-  return str.toLowerCase()
+function eatDocument (md, tokens, result) {
+  while (tokens.length > 0) {
+    eatSection(md, tokens, result)
+  }
+  return true
 }
+
+function eatSection (md, tokens, result) {
+  var left = []
+  var section = { }
+  if (!result.sections) result.sections = {}
+  result._section = section
+
+  // h
+  eatH1(md, tokens, result)
+  if (!eatHeading(md, tokens, result)) return
+
+  while (tokens.length) {
+    if (eatSection(md, tokens, result)) {
+      close()
+      return true
+    }
+    left.push(tokens.shift())
+  }
+
+  close()
+  return true
+
+  function close () {
+    section.content = md.renderer.render(left, md.options)
+  }
+}
+
+function eatH1 (md, tokens, result) {
+  var token = tokens[0]
+  if (!token) return false
+  if (token.type !== 'heading_open' || token.tag !== 'h1') return false
+
+  var textToken = tokens[1]
+  var txt = md.renderer.render(tokens[1].children, md.options)
+  result.name = txt
+  tokens.shift()
+  tokens.shift()
+  tokens.shift()
+  return true
+}
+
+function eatHeading (md, tokens, result) {
+  var token = tokens[0]
+  if (!token) return false
+  if (token.type !== 'heading_open') return false
+  if (!(~['h1', 'h2', 'h3'].indexOf(token.tag))) return false
+
+  var textToken = tokens[1]
+  var txt = md.renderer.render(tokens[1].children)
+  var section = result._section
+  section.name = txt
+  section.depth = token.tag
+
+  result.sections[txt] = section
+  tokens.shift()
+  tokens.shift()
+  tokens.shift()
+  return true
+}
+
+/*
+ * while (keep splicing) {
+ *   if eatSpecial()
+ *     process the spliced
+ *     shift the tokens
+ * }
+ */
--- test.js
+++ test.js
@@ -19,3 +19,19 @@ test('hello', t => {
   console.log(require('util').inspect(out, { depth: null }))
   t.pass()
 })
+
+test('multiple blocks', t => {
+  var out = styledown([
+    { name: 'components.md',
+      data: r(`
+        # Components
+        ### header
+        This is a header
+
+        ### footer
+        This is a footer
+      `) }
+  ])
+  console.log(require('util').inspect(out, { depth: null }))
+  t.pass()
+})
