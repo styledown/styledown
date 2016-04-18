05eda4ee47eac60669f88c9ecf5ea15556b366e1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-04-18 17:21:43 +0800

    Trim contents

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ test('block with code', t => {
   t.truthy(out.files['components.md'].sections.header.title === 'header')
   t.truthy(out.files['components.md'].sections.header.depth === 3)
   t.truthy(out.files['components.md'].sections.header.parts.s1.type === 'text')
-  t.truthy(out.files['components.md'].sections.header.parts.s1.content === '<p>This is a header</p>\n')
+  t.truthy(out.files['components.md'].sections.header.parts.s1.content === '<p>This is a header</p>')
   t.truthy(out.files['components.md'].sections.header.parts.s2.type === 'example')
   t.regex(out.files['components.md'].sections.header.parts.s2.content, /= render 'header'/)
   t.pass()
