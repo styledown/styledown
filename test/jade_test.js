02b94e20f8ddfb76e04da7f8e73f397f9677a490
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-04-19 23:40:25 +0800

    Fix transforms

--- test/jade_test.js
+++ test/jade_test.js
@@ -2,7 +2,7 @@ var test = require('ava')
 var styledown = require('../index')
 var r = require('redent')
 
-test('jade', t => {
+test('transforming jade', t => {
   var out = styledown.parse([
     { name: 'components.md',
       data: r(`
@@ -19,4 +19,23 @@ test('jade', t => {
   var example = out.files['components.md'].sections.header.parts.s2
   t.true(example.language === 'html')
   t.true(example.content === '<div class="hello">world</div>')
+})
+
+test('dont transform if not specified', t => {
+  var out = styledown.parse([
+    { name: 'components.md',
+      data: r(`
+        # Components
+        ### header
+        This is a header
+
+        ~~~ example.jade
+        .hello world
+        ~~~
+      `) }
+  ])
+
+  var example = out.files['components.md'].sections.header.parts.s2
+  t.true(example.language === 'jade')
+  t.true(example.content === '.hello world')
 })
