7a76adc14b1fb9f11809ba322863180dccb02b81
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-04-20 16:51:20 +0800

    Adjust styles

--- data/styledown.js
+++ data/styledown.js
@@ -1,23 +1,69 @@
 void (function () {
+  ready(function () {
+    var examples = document.querySelectorAll('.styleguide-example')
+
+    for (var i = 0, len = examples.length; i < len; i++) {
+      decorateExample(examples[i])
+    }
+  })
+
+  function decorateExample ($example) {
+    var $figure = $example.querySelector('.figure')
+    var $source = $example.querySelector('.source')
+    var collapsed
+
+    function collapse () {
+      addClass($example, '-collapse')
+      removeClass($example, '-expand')
+      collapsed = true
+    }
+
+    function expand () {
+      removeClass($example, '-collapse')
+      addClass($example, '-expand')
+      collapsed = false
+    }
+
+    function toggle () {
+      return collapsed ? expand() : collapse()
+    }
+
+    $figure.addEventListener('click', function () { toggle() })
+    collapse()
+  }
+
+  /*
+   * Helpers
+   */
+
   function ready (fn) {
     if (document.readyState === 'complete') {
-      return fn();
+      return fn()
     } else if (document.addEventListener) {
-      document.addEventListener('DOMContentLoaded', fn);
+      document.addEventListener('DOMContentLoaded', fn)
     } else {
       document.attachEvent('onreadystatechange', function () {
-        if (document.readyState === 'interactive') fn();
-      });
+        if (document.readyState === 'interactive') fn()
+      })
     }
   }
 
-  ready(function () {
-    var examples = document.querySelectorAll('.styleguide-section > .figure')
-
-    for (var i = 0, len = examples.length; i < len; i++) {
-      examples[i].addEventListener('click', function () {
-        alert('ok')
-      })
+  function addClass (el, className) {
+    if (el.classList) {
+      el.classList.add(className);
+    } else {
+      el.className += ' ' + className;
     }
-  })
+  }
+
+  function removeClass (el, className) {
+    if (el.classList) {
+      el.classList.remove(className);
+    } else {
+      var expr =
+        new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi');
+
+      el.className = el.className.replace(expr, ' ');
+    }
+  }
 }());
